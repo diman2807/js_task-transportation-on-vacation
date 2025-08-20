6130180b9ded833a25648266b1056f9d0adb50ec
Dima <dima2003strik@gmail.com>
2025-08-20 18:34:35 +0300

    resolve the problems

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -8,17 +8,18 @@ const SHORT_TERM = 3;
 const SHORT_TERM_DISCOUNT = 20;
 const LONG_TERM = 7;
 const LONG_TERM_DISCOUNT = 50;
+const DAILY_COST = 40;
 
 function calculateRentalCost(days) {
-  const oneDayCost = 40;
-
   if (days < SHORT_TERM) {
-    return oneDayCost * days;
-  } else if (days < LONG_TERM) {
-    return oneDayCost * days - SHORT_TERM_DISCOUNT;
-  } else {
-    return oneDayCost * days - LONG_TERM_DISCOUNT;
+    return (DAILY_COST * days);
   }
+
+  else if (days < LONG_TERM) {
+    return (DAILY_COST * days) - SHORT_TERM_DISCOUNT;
+  }
+
+  return (DAILY_COST * days) - LONG_TERM_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
